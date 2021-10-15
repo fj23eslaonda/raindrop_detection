a6a07154b932646561f1c94fa0d9c14d7ba49561
fj23eslaonda <64944609+fj23eslaonda@users.noreply.github.com>
2021-10-15 16:07:24 +0200

    Add files via upload

---
+++ model1/test_model.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 18-7-19 上午10:28
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/attentive-gan-derainnet
+# @File    : test_model.py
+# @IDE: PyCharm
+"""
+test model
+"""
+import os.path as ops
+import argparse
+import skimage.measure
+import tensorflow as tf
+import numpy as np
+import cv2
+from skimage.measure import compare_ssim
+from skimage.measure import compare_psnr
+from sklearn.metrics import log_loss
+
+import derain_drop_net
+import global_config
+
+CFG = global_config.cfg
+
+
+def init_args():
+    """
+
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_path', type=str, help='The input image path')
+    parser.add_argument('--weights_path', type=str, help='The model weights path')
+    parser.add_argument('--label_path', type=str, default=None, help='The label image path')
+
+    return parser.parse_args()
+
+
+def minmax_scale(input_arr):
+    """
+
+    :param input_arr:
+    :return:
+    """
+    min_val = np.min(input_arr)
+    max_val = np.max(input_arr)
+
+    output_arr = (input_arr - min_val) * 255.0 / (max_val - min_val)
+
+    return output_arr
+
+
+def visualize_attention_map(attention_map):
+    """
+    The attention map is a matrix ranging from 0 to 1, where the greater the value,
+    the greater attention it suggests
+    :param attention_map:
+    :return:
+    """
+    attention_map_color = np.zeros(
+        shape=[attention_map.shape[0], attention_map.shape[1], 3],
+        dtype=np.uint8
+    )
+
+    red_color_map = np.zeros(
+        shape=[attention_map.shape[0], attention_map.shape[1]],
+        dtype=np.uint8) + 255
+    red_color_map = red_color_map * attention_map
+    red_color_map = np.array(red_color_map, dtype=np.uint8)
+
+    attention_map_color[:, :, 2] = red_color_map
+
+    return attention_map_color
+
+
+def test_model(image_path, weights_path, label_path=None):
+    """
+
+    :param image_path:
+    :param weights_path:
+    :param label_path:
+    :return:
+    """
+    assert ops.exists(image_path)
+
+    input_tensor = tf.placeholder(dtype=tf.float32,
+                                  shape=[CFG.TEST.BATCH_SIZE, CFG.TEST.IMG_HEIGHT, CFG.TEST.IMG_WIDTH, 3],
+                                  name='input_tensor'
+                                  )
+
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    image = cv2.resize(image, (CFG.TEST.IMG_WIDTH, CFG.TEST.IMG_HEIGHT), interpolation=cv2.INTER_LINEAR)
+    image_vis = image
+    image = np.divide(np.array(image, np.float32), 127.5) - 1.0
+
+    label_image_vis = None
+    if label_path is not None:
+        label_image = cv2.imread(label_path, cv2.IMREAD_COLOR)
+        label_image_vis = cv2.resize(
+            label_image, (CFG.TEST.IMG_WIDTH, CFG.TEST.IMG_HEIGHT), interpolation=cv2.INTER_LINEAR
+        )
+
+    phase = tf.constant('test', tf.string)
+
+    net = derain_drop_net.DeRainNet(phase=phase)
+    output, attention_maps = net.inference(input_tensor=input_tensor, name='derain_net')
+
+    # Set sess configuration
+    sess_config = tf.ConfigProto(allow_soft_placement=True)
+    sess_config.gpu_options.per_process_gpu_memory_fraction = CFG.TEST.GPU_MEMORY_FRACTION
+    sess_config.gpu_options.allow_growth = CFG.TEST.TF_ALLOW_GROWTH
+    sess_config.gpu_options.allocator_type = 'BFC'
+
+    sess = tf.Session(config=sess_config)
+
+    saver = tf.train.Saver()
+
+    with sess.as_default():
+        saver.restore(sess=sess, save_path=weights_path)
+
+        output_image, atte_maps = sess.run(
+            [output, attention_maps],
+            feed_dict={input_tensor: np.expand_dims(image, 0)})
+
+        output_image = output_image[0]
+
+        for i in range(output_image.shape[2]):
+            output_image[:, :, i] = minmax_scale(output_image[:, :, i])
+
+        output_image = np.array(output_image, np.uint8)
+
+        if label_path is not None:
+            label_image_vis_gray = cv2.cvtColor(label_image_vis, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
+            output_image_gray = cv2.cvtColor(output_image, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
+            psnr = compare_psnr(label_image_vis_gray, output_image_gray)
+            ssim = compare_ssim(label_image_vis_gray, output_image_gray)
+
+            print('SSIM: {:.5f}'.format(ssim))
+            print('PSNR: {:.5f}'.format(psnr))
+
+            # 保存并可视化结
+            # cv2.imwrite('src_img.png', image_vis)
+        #atte_maps = cv2.resize(atte_maps[3][0, :, :, 0],(3840,2160), interpolation = cv2.INTER_LINEAR)
+        #cv2.imwrite('./attention_map_model1/atte_map_'+args.image_path[29:34]+'.png', atte_maps*255)
+
+        #atte_maps = cv2.imread('./attention_map_model1/atte_map_'+args.image_path[29:34]+'.png')
+        #atte_maps = cv2.applyColorMap(atte_maps, cv2.COLORMAP_JET)
+        #cv2.imwrite('./attention_map_model1/atte_map_' + args.image_path[29:34] + '.png', atte_maps)
+
+        # plt.figure('src_image')
+        # plt.imshow(image_vis[:, :, (2, 1, 0)])
+        # plt.figure('derain_ret')
+        # plt.imshow(output_image[:, :, (2, 1, 0)])
+        # plt.figure('atte_map_1')
+        # plt.imshow(atte_maps[0][0, :, :, 0], cmap='jet')
+        # plt.savefig('atte_map_1.png')
+        # plt.figure('atte_map_2')
+        # plt.imshow(atte_maps[1][0, :, :, 0], cmap='jet')
+        # plt.savefig('atte_map_2.png')
+        # plt.figure('atte_map_3')
+        # plt.imshow(atte_maps[2][0, :, :, 0], cmap='jet')
+        # plt.savefig('atte_map_3.png')
+        # plt.figure('atte_map_4')
+        #plt.imshow(atte_maps[3][0, :, :, 0], cmap='jet')
+        #plt.savefig('atte_map_4.png')
+        #plt.show()
+
+    return
+
+
+if __name__ == '__main__':
+    # init args
+    args = init_args()
+
+    # test model
+    test_model(args.image_path, args.weights_path, args.label_path)
---
+++ models/model.py
@@ -0,0 +1,317 @@
+import os
+import time
+import random
+import datetime
+import scipy.misc
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import tensorflow.contrib.slim as slim
+from datetime import datetime
+from skimage.transform import resize
+from utils import *  # get_batch decode
+from metrics import *
+import imageio
+
+
+class RainDropRemoval(object):
+    def __init__(self, args):
+        self.args = args
+        self.chns = 3
+        self.scale = 0.5
+        self.crop_size = 256
+        self.n_levels = 1
+
+        self.data_list = open(args.datalist, 'rt').read().splitlines()
+        self.data_list = list(map(lambda x: x.split(' '), self.data_list))
+        print(self.data_list)
+        self.batch_size = args.batch_size
+        self.learning_rate = args.learning_rate
+        self.data_size = (len(self.data_list)) // self.batch_size
+        self.epoch = args.epoch
+        self.max_steps = int(self.epoch * self.data_size)
+        self.current_epoch = 0
+        self.iscale = 0
+        self.mode = 0
+        self.model_name = 'rainDrop.model'
+        self.train_dir = './checkpoints/'
+        self.restore_step = args.restore_step
+        if not os.path.exists(self.train_dir):
+            os.makedirs(self.train_dir)
+        self.tfrecord_dir = args.tfrecord_dir
+        print(self.batch_size, self.learning_rate, self.data_size, self.epoch, self.max_steps,
+              self.train_dir, self.tfrecord_dir)
+
+    def input_producer(self, batch_size=10):
+        def read_data():
+            img_a = tf.image.decode_image(tf.read_file(tf.string_join(['./', self.data_queue[0]])),
+                                          channels=3)
+            img_b = tf.image.decode_image(tf.read_file(tf.string_join(['./', self.data_queue[1]])),
+                                          channels=3)
+            img_a = tf.cast(img_a, tf.float32) / 255.0
+            img_b = tf.cast(img_b, tf.float32) / 255.0
+            img_a, img_b = tf.unstack(
+                tf.random_crop(tf.stack([img_a, img_b], axis=0), [2, self.crop_size, self.crop_size, self.chns]),
+                axis=0)
+
+            return img_a, img_b
+
+        with tf.variable_scope('input'):
+            List_all = tf.convert_to_tensor(self.data_list, dtype=tf.string)
+            gt_list = List_all[:, 0]
+            in_list = List_all[:, 1]
+
+            self.data_queue = tf.train.slice_input_producer([in_list, gt_list], capacity=20)
+            print("joint path")
+            image_in, image_gt = read_data()
+            batch_in, batch_gt = tf.train.batch([image_in, image_gt], batch_size=batch_size, num_threads=1, capacity=20)
+        return batch_in, batch_gt
+
+    def generatorSimplified(self, inputs, reuse=False, scope='g_net'):
+        n, h, w, c = inputs.get_shape().as_list()
+
+        with tf.variable_scope(scope, reuse=reuse):
+            with slim.arg_scope([slim.conv2d, slim.conv2d_transpose],
+                                activation_fn=tf.nn.relu, padding='SAME', normalizer_fn=None,
+                                weights_initializer=tf.contrib.layers.xavier_initializer(uniform=True),
+                                biases_initializer=tf.constant_initializer(0.0)):
+                inp_pred = inputs
+                inp_rdrop = tf.image.resize_images(inputs, [h, w], method=0)
+                inp_pred = tf.stop_gradient(tf.image.resize_images(inp_pred, [h, w], method=0))
+
+                edgemap = amitedgefinder(inp_pred, 10)
+                edgemap = tf.cast(edgemap, dtype=tf.float32)
+                attention = conv_torque(edgemap)
+                dot_product = inp_rdrop * attention
+                inp_all = tf.concat([inp_rdrop, dot_product, inp_pred], axis=3, name='inp')
+
+                # encoder
+
+                attention = slim.conv2d(attention, 32, [5, 5], scope='att1_1')
+                conv1_1 = slim.conv2d(inp_all, 32, [5, 5], scope='enc1_1')
+
+                conv1_2, _, _ = ResnetBlock_att(conv1_1, 32, 5, attention, scope='enc1_2')
+                conv1_3, _, _ = ResnetBlock_att(conv1_2, 32, 5, attention, scope='enc1_3')
+                conv1_4, _, _ = ResnetBlock_att(conv1_3, 32, 5, attention, scope='enc1_4')
+                conv2_1 = slim.conv2d(conv1_4, 64, [5, 5], stride=2, scope='enc2_1')
+                attention128 = slim.conv2d(attention, 64, [5, 5], stride=2, scope='att2_1')
+
+                conv2_2, _, _ = ResnetBlock_att(conv2_1, 64, 5, attention128, scope='enc2_2')
+                conv2_3, _, _ = ResnetBlock_att(conv2_2, 64, 5, attention128, scope='enc2_3')
+                conv2_4, _, _ = ResnetBlock_att(conv2_3, 64, 5, attention128, scope='enc2_4')
+                conv3_1 = slim.conv2d(conv2_4, 128, [5, 5], stride=2, scope='enc3_1')
+                attention64 = slim.conv2d(attention128, 128, [5, 5], stride=2, scope='att3_1')
+
+                conv3_2, _, _ = ResnetBlock_att(conv3_1, 128, 5, attention64, scope='enc3_2')
+                conv3_3, _, _ = ResnetBlock_att(conv3_2, 128, 5, attention64, scope='enc3_3')
+                conv3_4, _, _ = ResnetBlock_att(conv3_3, 128, 5, attention64, scope='enc3_4')
+
+                deconv3_4 = conv3_4
+
+                # decoder
+                deconv3_3, _, _ = ResnetBlock_att(deconv3_4, 128, 5, attention64, scope='dec3_3')
+                deconv3_2, _, _ = ResnetBlock_att(deconv3_3, 128, 5, attention64, scope='dec3_2')
+                deconv3_1, _, _ = ResnetBlock_att(deconv3_2, 128, 5, attention64, scope='dec3_1')
+                deconv2_4 = slim.conv2d_transpose(deconv3_1, 64, [4, 4], stride=2, scope='dec2_4')
+                cat2 = deconv2_4 + conv2_4
+                deconv2_3, _, _ = ResnetBlock_att(cat2, 64, 5, attention128, scope='dec2_3')
+                deconv2_2, _, _ = ResnetBlock_att(deconv2_3, 64, 5, attention128, scope='dec2_2')
+                deconv2_1, _, _ = ResnetBlock_att(deconv2_2, 64, 5, attention128, scope='dec2_1')
+                deconv1_4 = slim.conv2d_transpose(deconv2_1, 32, [4, 4], stride=2, scope='dec1_4')
+                cat1 = deconv1_4 + conv1_4
+                deconv1_3, _, _ = ResnetBlock_att(cat1, 32, 5, attention, scope='dec1_3')
+                deconv1_2, _, _ = ResnetBlock_att(deconv1_3, 32, 5, attention, scope='dec1_2')
+                deconv1_1, _, _ = ResnetBlock_att(deconv1_2, 32, 5, attention, scope='dec1_1')
+                inp_pred = slim.conv2d(deconv1_1, self.chns, [5, 5], activation_fn=None, scope='dec1_0')
+
+            return inp_pred
+
+    def build_model(self):
+        img_in, img_gt = self.input_producer(self.batch_size)
+
+        tf.summary.image('img_in', im2uint8(img_in))
+        tf.summary.image('img_gt', im2uint8(img_gt))
+        print('img_in, img_gt', img_in.get_shape(), img_gt.get_shape())
+
+        x_unwarp = self.generatorSimplified(img_in, reuse=False)
+
+        self.loss_total = 0
+
+        _, hi, wi, _ = x_unwarp.get_shape().as_list()
+        gt_i = tf.image.resize_images(img_gt, [hi, wi], method=0)
+
+        loss = tf.reduce_mean(tf.abs(gt_i - x_unwarp))
+        self.loss_total += loss
+
+        all_vars = tf.trainable_variables()
+        self.all_vars = all_vars
+        self.g_var = [var for var in all_vars if 'g_net' in var.name]
+        for var in all_vars:
+            print(var.name)
+
+    def train(self):
+        def get_optimizer(loss, global_step=None, var_list=None, is_gradient_clip=False):
+            train_op = tf.train.AdamOptimizer(self.lr)
+            if is_gradient_clip:
+                pass
+            else:
+                train_op = train_op.minimize(loss, global_step, var_list)
+            return train_op
+
+        global_step = tf.Variable(initial_value=self.restore_step, dtype=tf.int32, trainable=False)
+        self.global_step = global_step
+
+        # build model
+
+        self.build_model()
+        self.lr = tf.train.polynomial_decay(self.learning_rate, global_step, self.max_steps,
+                                            end_learning_rate=0.0, power=0.3)
+        tf.summary.scalar('learning_rate', self.lr)
+
+        train_gnet = get_optimizer(self.loss_total, global_step, self.g_var)
+
+        # session and thread
+        gpu_options = tf.GPUOptions(allow_growth=True)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        self.sess = sess
+        self.saver = tf.train.Saver(max_to_keep=100, keep_checkpoint_every_n_hours=1)
+
+        dir = './checkpoints/'
+        sess.run(tf.global_variables_initializer())
+
+        if self.restore_step != 0:
+            self.load(sess, dir, step=self.restore_step)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        for step in range(sess.run(global_step), self.max_steps + 1):
+            start_time = time.time()
+            _, loss_total_val = sess.run([train_gnet, self.loss_total])
+            duration = time.time() - start_time
+            # print loss value
+            assert not np.isnan(loss_total_val), 'Model diverged with loss = NaN'
+
+            self.current_epoch = step / self.data_size
+            if step % 10 == 0:
+                num_examples_per_step = self.batch_size
+                examples_per_sec = num_examples_per_step / duration
+                sec_per_batch = float(duration)
+                format_str = (
+                    '%s: step %d, final_step %d, epoch %d, lr %.5f, loss = (%.5f; %.5f, %.5f)(%.1f data/s; %.3f s/bch)')
+                print(format_str % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), step, self.max_steps,
+                                    self.current_epoch, sess.run(self.lr), loss_total_val, 0.0, 0.0, examples_per_sec,
+                                    sec_per_batch))
+
+            if step > self.restore_step and step % 5000 == 0 and step > 10000 or step == self.max_steps:
+                checkpoint_path = os.path.join(self.train_dir, 'checkpoints')
+                self.save(sess, checkpoint_path, step)
+
+    def save(self, sess, checkpoint_dir, step):
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        self.saver.save(sess, os.path.join(checkpoint_dir, self.model_name), global_step=step)
+
+    def load(self, sess, checkpoint_dir, step=None):
+        print(' [*] Reading checkpoints...')
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+
+        if step is not None:
+            ckpt_name = self.model_name + '-' + str(step)
+            self.saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
+            print(' [*] Reading intermediate checkpoints... Success')
+            return str(step)
+        elif ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            ckpt_iter = ckpt_name.split('-')[1]
+            self.saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
+            print(' [*] Reading checkpoints... Success')
+            return ckpt_iter
+        else:
+            print(' [*] Reading checkpoints... ERROR')
+            return False
+
+    def test(self, inputdata_path, output_path):
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+            os.makedirs('./testing_result/attention_map')
+        input_path = inputdata_path  # + '/data'
+        # gt_path = inputdata_path + '/gt'
+        imgsName = sorted(os.listdir(input_path))
+        # gtsName  = sorted(os.listdir(gt_path))
+        num = len(imgsName)
+        H, W = 480, 720
+        inp_chns = 3
+        self.batch_size = 1
+        inputs = tf.placeholder(shape=[self.batch_size, H, W, inp_chns], dtype=tf.float32)
+        outputs = self.generatorSimplified(inputs, reuse=False)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
+
+        self.saver = tf.train.Saver()
+        dir = './checkpoints/'
+        self.load(sess, dir, step=self.restore_step)
+
+        total_psnr = 0
+        total_rgbpsnr = 0
+        total_ssim = 0
+
+        for i in range(num):
+            # gt = imageio.imread(os.path.join(gt_path, gtsName[i]))
+            rDrop = imageio.imread(os.path.join(input_path, imgsName[i]))
+            h, w, c = rDrop.shape
+            rDrop = rDrop[:, :, 0:3]
+            print(rDrop.shape)
+            # make sure the width is larger than the height
+            rot = False
+            if h > w:
+                rDrop = np.transpose(rDrop, [1, 0, 2])
+                rot = True
+            h = int(rDrop.shape[0])
+            w = int(rDrop.shape[1])
+            resize = False
+            if h > H or w > W:
+                scale = min(1.0 * H / h, 1.0 * W / w)
+                new_h = int(h * scale)
+                new_w = int(w * scale)
+                rDrop = scipy.misc.imresize(rDrop, [new_h, new_w], 'bicubic')
+                resize = True
+                rDropPad = np.pad(rDrop, ((0, H - new_h), (0, W - new_w), (0, 0)), 'edge')
+            else:
+                rDropPad = np.pad(rDrop, ((0, H - h), (0, W - w), (0, 0)), 'edge')
+            rDropPad = np.expand_dims(rDropPad, 0)
+
+            start = time.time()
+            derDrop = sess.run(outputs, feed_dict={inputs: rDropPad/255.0})
+            #attention_map = sess.run(atte_map, feed_dict={inputs: rDropPad/255.0})
+            duration = time.time() - start
+
+            #attention_map = resize(attention_map[0,:,:,0], [2160, 3840], 'bicubic')
+            print('Saving results: %s ... %4.3fs' % (os.path.join(output_path, imgsName[i]), duration))
+
+            res = im2uint8(derDrop[0, :, :, :])
+
+            # crop the image into original size
+            if resize:
+                res = res[:new_h, :new_w]
+                res = scipy.misc.imresize(res, [h, w], 'bicubic')
+            else:
+                res = res[:h, :w, :]
+            if rot:
+                res = np.transpose(res, [1, 0, 2])
+            imageio.imwrite(os.path.join(output_path, imgsName[i]), res)
+            #imageio.imwrite(os.path.join("./testing_result/attention_map", imgsName[i]), attention_map)
+        # res = align_to_four(res)
+        # gt = align_to_four(gt)
+
+        # ssim = calc_ssim(res, gt)
+        # psnr = calc_psnr(res, gt)
+        # rgbpsnr = psnrrgb(res,gt)
+        # print('picture ' + str(i) + '\'s psnr:', psnr)
+        # print('picture ' + str(i) + '\'s psnr(rgb):', rgbpsnr)
+        # print('picture ' + str(i) + '\'s ssim:', ssim)
+        # total_psnr += psnr
+        # total_rgbpsnr +=rgbpsnr
+        # total_ssim += ssim
+    # print('average psnr:', total_psnr / num)
+    # print('average psnr(rgb):', total_rgbpsnr / num)
+    # print('average ssim:', total_ssim / num)
